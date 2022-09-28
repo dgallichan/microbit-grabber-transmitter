9b9d78ec353ccbf1d576b64c4d435fe7810857ce
Daniel Gallichan <dgallichan@yahoo.com>
2022-09-28 16:05:44 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -12,13 +12,21 @@ basic.forever(function () {
         radio.sendValue("Drive", Math.map(input.rotation(Rotation.Pitch), -90, 90, 1000, -1000))
         radio.sendValue("Turn", Math.map(input.rotation(Rotation.Roll), -90, 90, -1000, 1000))
         radio.sendValue("Grabber", -9999)
+        radio.sendValue("Spin", -9999)
     } else if (input.buttonIsPressed(Button.B)) {
         radio.sendValue("Drive", 0)
         radio.sendValue("Turn", 0)
         radio.sendValue("Grabber", Math.map(input.rotation(Rotation.Pitch), -90, 90, 0, 1000))
+        radio.sendValue("Spin", -9999)
+    } else if (input.logoIsPressed()) {
+        radio.sendValue("Drive", 0)
+        radio.sendValue("Turn", 0)
+        radio.sendValue("Grabber", -9999)
+        radio.sendValue("Spin", Math.map(input.rotation(Rotation.Roll), -90, 90, -1000, 1000))
     } else {
         radio.sendValue("Drive", 0)
         radio.sendValue("Turn", 0)
         radio.sendValue("Grabber", -9999)
+        radio.sendValue("Spin", -9999)
     }
 })
