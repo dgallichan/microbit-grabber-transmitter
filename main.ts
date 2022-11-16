8e4919fae769d3ea13fe7a687f3c1d06fb7f3251
Daniel Gallichan <dgallichan@yahoo.com>
2022-11-16 11:38:20 +0000

    Now using -90 to +90 as the range, and -999 as 'ignore'

--- main.ts
+++ main.ts
@@ -1,4 +1,39 @@
-radio.setGroup(1)
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(groupNumber)
+    makingChoice = true
+    while (makingChoice) {
+        basic.pause(200)
+        if (input.logoIsPressed()) {
+            makingChoice = false
+            if (groupNumber <= -1) {
+                groupNumber = groupNumber + 256
+            }
+            if (groupNumber >= 256) {
+                groupNumber = groupNumber - 256
+            }
+            radio.setGroup(groupNumber)
+            basic.showIcon(IconNames.Yes)
+            basic.pause(200)
+            basic.showLeds(`
+                . # # # .
+                . . # . .
+                . . # . .
+                # . . . #
+                . # # # .
+                `)
+        } else if (input.buttonIsPressed(Button.A)) {
+            groupNumber = groupNumber - 1
+            basic.showNumber(groupNumber)
+        } else if (input.buttonIsPressed(Button.B)) {
+            groupNumber = groupNumber + 1
+            basic.showNumber(groupNumber)
+        }
+    }
+})
+let makingChoice = false
+let groupNumber = 0
+groupNumber = 0
+radio.setGroup(groupNumber)
 basic.showLeds(`
     . # # # .
     . . # . .
@@ -9,24 +44,24 @@ basic.showLeds(`
 basic.forever(function () {
     if (input.buttonIsPressed(Button.A)) {
         // pitch downwards is a negative value, and this corresponds to what we want to mean 'forwards'
-        radio.sendValue("Drive", Math.map(input.rotation(Rotation.Pitch), -90, 90, 1000, -1000))
-        radio.sendValue("Turn", Math.map(input.rotation(Rotation.Roll), -90, 90, -1000, 1000))
-        radio.sendValue("Grabber", -9999)
-        radio.sendValue("Spin", -9999)
+        radio.sendValue("Drive", input.rotation(Rotation.Pitch) * -1)
+        radio.sendValue("Turn", input.rotation(Rotation.Roll))
+        radio.sendValue("Grabber", -999)
+        radio.sendValue("Spin", -999)
     } else if (input.buttonIsPressed(Button.B)) {
         radio.sendValue("Drive", 0)
         radio.sendValue("Turn", 0)
-        radio.sendValue("Grabber", Math.map(input.rotation(Rotation.Pitch), -90, 90, 0, 1000))
-        radio.sendValue("Spin", -9999)
+        radio.sendValue("Grabber", input.rotation(Rotation.Pitch))
+        radio.sendValue("Spin", -999)
     } else if (input.logoIsPressed()) {
         radio.sendValue("Drive", 0)
         radio.sendValue("Turn", 0)
-        radio.sendValue("Grabber", -9999)
-        radio.sendValue("Spin", Math.map(input.rotation(Rotation.Roll), -90, 90, -1000, 1000))
+        radio.sendValue("Grabber", -999)
+        radio.sendValue("Spin", input.rotation(Rotation.Roll))
     } else {
         radio.sendValue("Drive", 0)
         radio.sendValue("Turn", 0)
-        radio.sendValue("Grabber", -9999)
-        radio.sendValue("Spin", -9999)
+        radio.sendValue("Grabber", -999)
+        radio.sendValue("Spin", -999)
     }
 })
